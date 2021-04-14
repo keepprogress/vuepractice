39dcb50b8d5b90963181cd4eeb59068f6d8bea27
getzch <keepprogress@gmail.com>
2021-04-14 15:13:31 +0800

    test json array is already in App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,89 @@ export default {
   },
   data () {
     return {
+      infoAndOrderArrar: [
+        {
+          id: 'A0000001',
+          personal_info:
+            {
+              first_name: 'Fishman',
+              last_name: 'ILike',
+              gender: 1,
+              address: 'ABCDEFG',
+              is_homeless: false,
+              job: null,
+              note: null
+            },
+          orders: {
+            apple_count: 1,
+            banana_condiments: ['chocolate', 'chili', 'garlic', 'soy_sauce']
+          }
+        },
+        {
+          id: 'A0000002',
+          personal_info: {
+            first_name: null,
+            last_name: 'Somebody',
+            gender: 2,
+            address: 'QWERTY',
+            is_homeless: false,
+            job: 'secret_agent',
+            note: 'Hello, world'
+          },
+          orders: {
+            apple_count: 15,
+            banana_condiments: []
+          }
+        },
+        {
+          id: 'A0000003',
+          personal_info: {
+            first_name: 'Painter',
+            last_name: null,
+            gender: 0,
+            address: null,
+            is_homeless: true,
+            job: 'agent_of_secret_agent',
+            note: 'Strange'
+          },
+          orders: {
+            apple_count: 1,
+            banana_condiments: ['herbal_cream']
+          }
+        },
+        {
+          id: 'A0000004',
+          personal_info: {
+            first_name: 'President',
+            last_name: 'Mr.',
+            gender: 1,
+            address: 'America',
+            is_homeless: false,
+            job: 'secret_agent',
+            note: 'He is the president!'
+          },
+          orders: {
+            apple_count: 100,
+            banana_condiments: ['chocolate', 'strawberry', 'flax', 'miso', 'chili', 'garlic', 'soy_sauce', 'thick_soy_sauce']
+          }
+        },
+        {
+          id: 'A0000005',
+          personal_info: {
+            first_name: 'Pepper',
+            last_name: 'Dr.',
+            gender: 0,
+            address: 'Farm',
+            is_homeless: false,
+            job: 'agent',
+            note: '胡椒博士現身'
+          },
+          orders: {
+            apple_count: 5,
+            banana_condiments: ['chili', 'garlic']
+          }
+        }
+      ]
     }
   }
 }
