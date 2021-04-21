0737f0c39b2c97ef0360422083ca3f8474a50b0e
getzch <keepprogress@gmail.com>
2021-04-21 12:43:11 +0800

    renamed component_value to Orders

--- src/components/CartFormOrders.vue
+++ src/components/CartFormOrders.vue
@@ -28,39 +28,39 @@
     <div class="row">
       <h5>Banana</h5>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="chocolate" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="chocolate" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox1">巧克力醬</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="strawberry" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="strawberry" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox2">草莓醬</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="flax" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox3" value="flax" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox3">胡麻醬</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="miso" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="miso" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox4">味噌</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="chili" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox5" value="chili" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox5">辣椒</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox6" value="garlic" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox6" value="garlic" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox6">大蒜</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox7" value="soy_sauce" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox7" value="soy_sauce" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox7">醬油</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox8" value="thick_soy_sauce" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox8" value="thick_soy_sauce" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox8">醬油膏</label>
       </div>
       <div class="form-check form-check-inline">
-        <input class="form-check-input" type="checkbox" id="inlineCheckbox9" value="herbal_cream" v-model="component_value.banana_condiments">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox9" value="herbal_cream" v-model="Orders.banana_condiments">
         <label class="form-check-label" for="inlineCheckbox9">百草膏</label>
       </div>
     </div>
@@ -83,7 +83,7 @@ export default {
     }
   },
   computed: {
-    component_value () {
+    Orders () {
       return this.orders
     }
   },
